2dc2af26040c4c42f029f453b222c63a8c05d310
shani-patel-17 <shanipatel178@gmail.com>
2023-04-13 23:05:36 +0530

    DONE

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <nav className="navbar">
+            
             <Link to="/"><img src="./images/pokemonLogo.png" alt="logo" /></Link>
+            <h2>POKEMON</h2>
             {/* <Link className="menu-item" to="/pokemon">Pokemon</Link> */}
             
         </nav >
